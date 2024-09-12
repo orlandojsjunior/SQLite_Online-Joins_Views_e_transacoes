b3c7d129ede1be24349cbdc4631c916b635fa819
Orlando Junior <orlandojsjunior@hotmail.com>
2024-09-11 22:22:08 -0300

    Executando subconsultas

---
+++ Aula 2 - Atividade 06 - Executando subconsultas.sql
@@ -0,0 +1,8 @@
+-- Aula 2 - Atividade 06 - Executando subconsultas
+
+/*
+Buscamos agora informações sobre pedidos e clientes, mais especificamente, compreendendo quem foi o primeiro cliente a fazer um pedido
+no Serenatto Café e Bistrô neste ano, em 2023. Inicialmente, extraímos as informações dos clientes com SELECT * FROM clientes.
+*/
+
+SELECT * FROM clientes
